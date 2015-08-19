fdbaff5cb191cf74951a4d2c393e770548d7d691
Kunal Sharma <kunalsharma1221@gmail.com>
2015-08-20 00:01:18 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var config = require('./config');
+
+var app  = express();
+
+
+// Middlewares
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.get('*', function(req, res){
+	res.sendFile(__dirname + '/public/views/index.html');
+});
+
+app.listen(config.port, function(err){
+	if(err){
+		console.log(err);
+	}else{
+		console.log('Listening on port 3000');
+	}	
+})
