a7cf223c3574c322b1b8e74e73d860a576993b25
Kunal Sharma <kunalsharma1221@gmail.com>
2015-08-30 23:06:31 +0545

    changed link to database

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-	"database":"mongodb://root:rainrain13514@ds035573.mongolab.com:35573/nith-story",
+	"database":"mongodb://root:root13514@ds035603.mongolab.com:35603/nith-story",
 	"port": process.env.PORT || 3000,
 	"secretKey": "MySercetKey"
 }
