54d2c95ff00b2dcbee5a4c60cc99d24a423b2f3d
Kunal Sharma <kunalsharma1221@gmail.com>
2016-12-23 00:29:43 +0530

    changed secret key

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
 
 	"database":"mongodb://root:root13514@ds035603.mongolab.com:35603/nith-story",
 	"port": process.env.PORT || 3000,
-	"secretKey": "MySercetKey"
+	"secretKey": "changedsecret"
 }
